899000121d8c7201ec559ab18b79cb365de89997
Timi007 <timi007gp@gmail.com>
2018-10-21 03:08:42 +0200

    add option to show Presets UI by default

--- addons/markers/initSettings.hpp
+++ addons/markers/initSettings.hpp
@@ -9,3 +9,13 @@
         GVAR(lastSelection) = [];
     }
 ] call CBA_settings_fnc_init;
+
+[
+    QGVAR(showPresetsUI),
+    "CHECKBOX",
+    [LLSTRING(cba_settings_show_presets_UI), LLSTRING(cba_settings_show_presets_UI_tooltip)],
+    LLSTRING(cba_category_name),
+    false,
+    2,
+    {}
+] call CBA_settings_fnc_init;
