fbbc6939f7839697d6c5a29d50af8ecaa72ea8cc
Timi007 <timi007gp@gmail.com>
2021-09-21 17:32:45 +0200

    Add CBA versioning

--- addons/markers/script_macros.hpp
+++ addons/markers/script_macros.hpp
@@ -1,6 +1,5 @@
 #define DEBUG_SYNCHRONOUS
 #include "\x\cba\addons\main\script_macros_common.hpp"
-#include "\x\cba\addons\xeh\script_xeh.hpp"
 
 // Default versioning level
 #define DEFAULT_VERSIONING_LEVEL 2
