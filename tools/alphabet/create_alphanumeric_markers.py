37b90ac4be04866441338a44d0d862d61b229e62
Timi007 <timi007gp@gmail.com>
2022-08-21 15:13:59 +0200

    Code cleanup

--- tools/alphabet/create_alphanumeric_markers.py
+++ tools/alphabet/create_alphanumeric_markers.py
@@ -7,10 +7,11 @@ import pathlib
 import subprocess
 import shutil
 import argparse
+import timeit
 from PIL import Image, ImageFont, ImageDraw
 from tqdm.contrib.concurrent import thread_map
 
-if sys.platform == "win32":
+if sys.platform == 'win32':
     import winreg
 
 # Configuration
@@ -53,13 +54,13 @@ printable_char_sets = {
     'alphanum': {
         'characters': string.ascii_uppercase + string.digits,
         'substitutions': None,
-        'cfgNamePrefix': 'CfgMarkersAlphanumeric',
+        'cfg_name_prefix': 'CfgMarkersAlphanumeric',
         'get_macro': get_alphanum_macro,
     },
     'special': {
         'characters': ''.join(special_chars.keys()),
         'substitutions': special_chars,
-        'cfgNamePrefix': 'CfgMarkersSpecialChars',
+        'cfg_name_prefix': 'CfgMarkersSpecialChars',
         'get_macro': get_special_macro,
     }
 }
@@ -108,6 +109,8 @@ images_dir = os.path.join(project_dir, 'images')
 font_file = os.path.join(project_dir, 'RobotoMono', 'static', 'RobotoMono-SemiBold.ttf')
 font = ImageFont.truetype(font_file, size=35) # 26pt == 35px
 
+anchors = ['lb', 'rb']
+
 def create_image(letter, pos, anchor = 'lb'):
     img = Image.new('RGBA', img_size)
 
@@ -116,11 +119,11 @@ def create_image(letter, pos, anchor = 'lb'):
     letter_height = letter_size[3] - letter_size[1]
 
     # For some reason the width of the letter K is bigger then it should
-    if (letter == 'K'):
+    if letter == 'K':
         letter_width -= 1
 
     v_offset = text_v_offset
-    if (anchor == 'lb'):
+    if anchor == 'lb':
         offset_direction = -1
         v_offset += letter_width
     else:
@@ -134,21 +137,43 @@ def create_image(letter, pos, anchor = 'lb'):
 
     return img
 
+def create_all_images():
+    images = []
+    for set_name, alphabet in printable_char_sets.items():
+        for anchor in anchors:
+            for pos in range(num_of_letters):
+                export_dir = os.path.join(images_dir, set_name, anchor, str(pos))
+
+                for letter in alphabet['characters']:
+                    img = create_image(letter, pos, anchor)
+
+                    # Some chars are not allowed in file names, so replace them according
+                    # a substitution table if available
+                    if alphabet['substitutions'] is not None:
+                        letter = alphabet['substitutions'].get(letter, letter)
+
+                    file_name = f'mts_markers_{set_name}_{anchor}_{pos}_{letter}.png'
+                    export_file = os.path.join(export_dir, file_name)
+
+                    images.append((export_file, img))
+
+    return images
+
 def find_image_to_paa_tool():
     reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
     try:
-        k = winreg.OpenKey(reg, r"Software\bohemia interactive\arma 3 tools")
-        arma3tools_path = winreg.QueryValueEx(k, "path")[0]
+        k = winreg.OpenKey(reg, r'Software\bohemia interactive\arma 3 tools')
+        arma3tools_path = winreg.QueryValueEx(k, 'path')[0]
         winreg.CloseKey(k)
     except:
-        raise Exception("BadTools","Could not find Arma 3 Tools.")
+        raise Exception('BadTools', 'Could not find Arma 3 Tools.')
 
-    image_to_paa_path = os.path.join(arma3tools_path, "ImageToPAA", "ImageToPAA.exe")
+    image_to_paa_path = os.path.join(arma3tools_path, 'ImageToPAA', 'ImageToPAA.exe')
 
     if os.path.isfile(image_to_paa_path):
         return image_to_paa_path
     else:
-        raise Exception("BadTools", "Arma 3 Tools are not installed correctly.")
+        raise Exception('BadTools', 'Arma 3 Tools are not installed correctly.')
 
 def save_image_as_paa(image_info):
     global image_to_paa
@@ -162,7 +187,7 @@ def save_image_as_paa(image_info):
     file_name = os.path.basename(path)
     img.save(path)
 
-    if sys.platform == "win32" and not png_only:
+    if sys.platform == 'win32' and not png_only:
         subprocess.run([image_to_paa, path], stdout=subprocess.DEVNULL)
 
     if delete_png:
@@ -196,67 +221,11 @@ def write_valid_characters(file):
     variables_concat = ' + '.join(variables)
     file.write(f'GVAR(validCharacters) = {variables_concat};\n\n')
 
-def main(args):
-    global image_to_paa
-
-    print('Using font file:', font_file)
-
-    # dev_frame = os.path.join(project_dir, 'frameshapes', 'combined_frameshape.png')
-    # dev_img = Image.open(dev_frame)
-    # img = create_image('A', 0, 'lb')
-    # dev_img = Image.alpha_composite(dev_img, img)
-    # dev_img.show()
-
-    if sys.platform == "win32":
-        image_to_paa = find_image_to_paa_tool()
-        print('Found ImageToPaa Tool:', image_to_paa)
-    else:
-        print('Cannot find ImageToPaa on non-windows platform.')
-        print('Images will not be converted.')
-        delete_png = False
-
-    print('Creating images...')
-
-    images = []
-    for anchor in ['lb', 'rb']:
-        for pos in range(num_of_letters):
-            for set_name, alphabet in printable_char_sets.items():
-                export_dir = os.path.join(images_dir, set_name, anchor, str(pos))
-
-                for letter in alphabet['characters']:
-                    img = create_image(letter, pos, anchor)
-
-                    # Some chars are not allowed in file names, so replace them according
-                    # a substitution table if available
-                    if alphabet['substitutions'] is not None:
-                        letter = alphabet['substitutions'].get(letter, letter)
-
-                    file_name = f'mts_markers_{set_name}_{anchor}_{pos}_{letter}.png'
-                    export_file = os.path.join(export_dir, file_name)
-
-                    images.append((export_file, img))
-
-    print('Cleaning export folder:', clean_images)
-    if clean_images:
-        shutil.rmtree(pathlib.Path(images_dir))
-
-    print('Exporting only PNG files:', png_only)
-
-    print('Exporting images...')
-
-    # Multithreading
-    thread_map(save_image_as_paa, images)
-
-    print(f'Exported {len(images)} images')
-
-    print('Creating include files...')
-
+def write_include_files(verbose=False):
     includes = []
     for alphabet in printable_char_sets.values():
-        for anchor in ['lb', 'rb']:
-            anchorUpper = anchor.upper()
-            cfgPrefix = alphabet['cfgNamePrefix']
-            file_name = f'{cfgPrefix}{anchorUpper}.hpp'
+        for anchor in anchors:
+            file_name = f'{alphabet["cfg_name_prefix"]}{anchor.upper()}.hpp'
             includes.append(file_name)
 
             with open(os.path.join(images_dir, file_name), 'w') as f:
@@ -274,13 +243,64 @@ def main(args):
 
                     f.write('\n')
 
-            print(f'Created {file_name}')
+            if verbose:
+                print(f'Created {file_name}')
 
     with open(os.path.join(images_dir, 'CfgMarkersCharacters.hpp'), 'w') as f:
         for include in includes:
             f.write(f'#include \"{include}\"\n')
 
-    print('Created CfgMarkersCharacters.hpp')
+    if verbose:
+        print('Created CfgMarkersCharacters.hpp\n')
+
+def fract_sec(s):
+    temp = float(s) / (60 * 60 * 24)
+    d = int(temp)
+    temp = (temp - d) * 24
+    h = int(temp)
+    temp = (temp - h) * 60
+    m = int(temp)
+    temp = (temp - m) * 60
+    sec = temp
+
+    return d,h,m,sec
+
+def main(args):
+    global image_to_paa
+
+    start_time = timeit.default_timer()
+
+    print('Using font file:', font_file)
+
+    # dev_frame = os.path.join(project_dir, 'frameshapes', 'combined_frameshape.png')
+    # dev_img = Image.open(dev_frame)
+    # img = create_image('A', 0, 'lb')
+    # dev_img = Image.alpha_composite(dev_img, img)
+    # dev_img.show()
+
+    if sys.platform == 'win32':
+        image_to_paa = find_image_to_paa_tool()
+        print('Found ImageToPaa Tool:', image_to_paa)
+    else:
+        print('Cannot find ImageToPaa on non-windows platform.')
+        print('Images will not be converted.')
+        delete_png = False
+
+    print('Creating images...')
+    images = create_all_images()
+
+    print('Cleaning export folder:', clean_images)
+    if clean_images:
+        shutil.rmtree(pathlib.Path(images_dir))
+
+    print('Exporting only PNG files:', png_only)
+    print('Exporting images...')
+    # Convert images to paa with multithreading
+    thread_map(save_image_as_paa, images)
+    print(f'Exported {len(images)} images\n')
+
+    print('Creating include files...')
+    write_include_files(True)
 
     print('Generating SQF substitute tables and character variables...')
     with open(os.path.join(images_dir, 'initCharacterMarkerVariables.hpp'), 'w') as f:
@@ -288,6 +308,12 @@ def main(args):
 
         write_translation_table(f)
         write_valid_characters(f)
+
+    print('Created initCharacterMarkerVariables.hpp\n')
+
+    print('Finished generating character marker files.')
+    d,h,m,s = fract_sec(timeit.default_timer() - start_time)
+    print(f'Total program time elapsed: {h:2}h {m:2}m {s:4.3f}s')
 
 
 if __name__ == "__main__":
