f49cab86160c1cd038795b546b83b0c457016499
Timi007 <timi007gp@gmail.com>
2023-07-22 17:41:01 +0200

    Replace open last RPT script with extension

--- .vscode/openLatestRpt.ps1
+++
@@ -1,4 +0,0 @@
-$dir = Join-Path $env:LOCALAPPDATA "Arma 3"
-$rpt = Get-ChildItem -Path $dir -Filter *.rpt | Sort-Object LastWriteTime -Descending | Select-Object -First 1
-
-&code --reuse-window $rpt.FullName
