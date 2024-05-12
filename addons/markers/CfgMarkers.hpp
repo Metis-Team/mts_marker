3c7d573be19fe0ba97c75ef42056d991ab1452f7
Timi007 <timi007gp@gmail.com>
2024-05-12 16:30:37 +0200

    Add base marker classes

--- addons/markers/CfgMarkers.hpp
+++ addons/markers/CfgMarkers.hpp
@@ -1,5 +1,5 @@
 #define IDENTITY_MARKER(IDENTITY,MODIFIER,NAME) \
-class mts_##IDENTITY##_##MODIFIER##_##NAME##: mts_##IDENTITY##_frameshape { \
+class mts_##IDENTITY##_##MODIFIER##_##NAME##: mts_##IDENTITY##_base { \
     icon = QPATHTOF(data\IDENTITY\MODIFIER\mts_markers_##IDENTITY##_##MODIFIER##_##NAME##.paa); \
 }
 
@@ -12,29 +12,29 @@ class mts_##IDENTITY##_##MODIFIER##_##NAME##: mts_##IDENTITY##_frameshape { \
 
 // Common markers
 #define COM_MARKER(MODIFIER,NAME) \
-class mts_com_##MODIFIER##_##NAME##: mts_blu_frameshape { \
+class mts_com_##MODIFIER##_##NAME##: mts_blu_base { \
     icon = QPATHTOF(data\com\MODIFIER\mts_markers_com_##MODIFIER##_##NAME##.paa); \
 }
 
 // Character markers
 #define ALPHANUM_MARKER(ANCHOR,POSITION,LETTER) \
-class mts_alphanum_##ANCHOR##_##POSITION##_##LETTER##: mts_blu_frameshape { \
+class mts_alphanum_##ANCHOR##_##POSITION##_##LETTER##: mts_blu_base { \
     icon = QPATHTOF(data\alphanum\ANCHOR\POSITION\mts_markers_alphanum_##ANCHOR##_##POSITION##_##LETTER##.paa); \
 }
 #define SPECIAL_CHAR_MARKER(ANCHOR,POSITION,LETTER) \
-class mts_special_##ANCHOR##_##POSITION##_##LETTER##: mts_blu_frameshape { \
+class mts_special_##ANCHOR##_##POSITION##_##LETTER##: mts_blu_base { \
     icon = QPATHTOF(data\special\ANCHOR\POSITION\mts_markers_special_##ANCHOR##_##POSITION##_##LETTER##.paa); \
 }
 
 // Date-Time Group markers
 #define DTG_MARKER(POSITION,LETTER) \
-class mts_dtg_##POSITION##_##LETTER##: mts_blu_frameshape { \
+class mts_dtg_##POSITION##_##LETTER##: mts_blu_base { \
     icon = QPATHTOF(data\dtg\POSITION\mts_markers_dtg_##POSITION##_##LETTER##.paa); \
 }
 
 // Direction of Movement markers
 #define IDENTITY_DIR_MARKER(IDENTITY,DIRECTION) \
-class mts_##IDENTITY##_dir_##DIRECTION##: mts_##IDENTITY##_frameshape { \
+class mts_##IDENTITY##_dir_##DIRECTION##: mts_##IDENTITY##_base { \
     icon = QPATHTOF(data\IDENTITY\dir\mts_markers_##IDENTITY##_dir_##DIRECTION##.paa); \
 }
 #define DIR_MARKER(DIRECTION) \
@@ -45,7 +45,7 @@ class mts_##IDENTITY##_dir_##DIRECTION##: mts_##IDENTITY##_frameshape { \
 
 // Direction of Movement markers for modifications (e.g. HQ)
 #define IDENTITY_ALT_DIR_MARKER(IDENTITY,MOD,DIRECTION) \
-class mts_##IDENTITY##_dir_##MOD##_##DIRECTION##: mts_##IDENTITY##_frameshape { \
+class mts_##IDENTITY##_dir_##MOD##_##DIRECTION##: mts_##IDENTITY##_base { \
     icon = QPATHTOF(data\IDENTITY\dir\MOD\mts_markers_##IDENTITY##_dir_##MOD##_##DIRECTION##.paa); \
 }
 #define DIR_MOD_MARKER(MOD,DIRECTION) \
@@ -59,11 +59,30 @@ class CfgMarkers {
     class o_unknown;
     class n_unknown;
 
-    class mts_blu_frameshape: b_unknown {
+    // Base classes
+    class mts_blu_base: b_unknown {
         author = CSTRING(authors);
         scope = 0;
-        icon = QPATHTOF(data\blu\mts_markers_blu_frameshape.paa);
         size = 256;
+    };
+    class mts_red_base: o_unknown {
+        author = CSTRING(authors);
+        scope = 0;
+        size = 256;
+    };
+    class mts_neu_base: n_unknown {
+        author = CSTRING(authors);
+        scope = 0;
+        size = 256;
+    };
+    class mts_unk_base: n_unknown {
+        author = CSTRING(authors);
+        scope = 0;
+        size = 256;
+    };
+
+    class mts_blu_frameshape: mts_blu_base {
+        icon = QPATHTOF(data\blu\mts_markers_blu_frameshape.paa);
     };
     class mts_blu_vanilla_frameshape: mts_blu_frameshape {
         icon = QPATHTOF(data\blu\mts_markers_blu_vanilla_frameshape.paa);
@@ -74,14 +93,12 @@ class CfgMarkers {
     class mts_bludash_vanilla_frameshape: mts_bludash_frameshape {
         icon = QPATHTOF(data\blu\mts_markers_bludash_vanilla_frameshape.paa);
     };
-    class mts_blu_hq: mts_blu_frameshape {
+    class mts_blu_hq: mts_blu_base {
         icon = QPATHTOF(data\blu\mts_markers_blu_hq.paa);
     };
-    class mts_red_frameshape: o_unknown {
-        author = CSTRING(authors);
-        scope = 0;
+
+    class mts_red_frameshape: mts_red_base {
         icon = QPATHTOF(data\red\mts_markers_red_frameshape.paa);
-        size = 256;
     };
     class mts_red_vanilla_frameshape: mts_red_frameshape {
         icon = QPATHTOF(data\red\mts_markers_red_vanilla_frameshape.paa);
@@ -92,26 +109,22 @@ class CfgMarkers {
     class mts_reddash_vanilla_frameshape: mts_reddash_frameshape {
         icon = QPATHTOF(data\red\mts_markers_reddash_vanilla_frameshape.paa);
     };
-    class mts_red_hq: mts_red_frameshape {
+    class mts_red_hq: mts_red_base {
         icon = QPATHTOF(data\red\mts_markers_red_hq.paa);
     };
-    class mts_neu_frameshape: n_unknown {
-        author = CSTRING(authors);
-        scope = 0;
+
+    class mts_neu_frameshape: mts_neu_base {
         icon = QPATHTOF(data\neu\mts_markers_neu_frameshape.paa);
-        size = 256;
     };
     class mts_neu_vanilla_frameshape: mts_neu_frameshape {
         icon = QPATHTOF(data\neu\mts_markers_neu_vanilla_frameshape.paa);
     };
-    class mts_neu_hq: mts_neu_frameshape {
+    class mts_neu_hq: mts_neu_base {
         icon = QPATHTOF(data\neu\mts_markers_neu_hq.paa);
     };
-    class mts_unk_frameshape: n_unknown {
-        author = CSTRING(authors);
-        scope = 0;
+
+    class mts_unk_frameshape: mts_unk_base {
         icon = QPATHTOF(data\unk\mts_markers_unk_frameshape.paa);
-        size = 256;
     };
     class mts_unk_vanilla_frameshape: mts_unk_frameshape {
         icon = QPATHTOF(data\unk\mts_markers_unk_vanilla_frameshape.paa);
@@ -122,7 +135,7 @@ class CfgMarkers {
     class mts_unkdash_vanilla_frameshape: mts_unkdash_frameshape {
         icon = QPATHTOF(data\unk\mts_markers_unkdash_vanilla_frameshape.paa);
     };
-    class mts_unk_hq: mts_unk_frameshape {
+    class mts_unk_hq: mts_unk_base {
         icon = QPATHTOF(data\unk\mts_markers_unk_hq.paa);
     };
 
